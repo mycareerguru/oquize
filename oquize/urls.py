5ae6991039b94eb512a9ebb646e0bee3adbfccb6
Sagar R. Gosavi <sagargosavi1112@gmail.com>
2013-02-23 00:33:20 +0530

    Added logout page

--- oquize/urls.py
+++ oquize/urls.py
@@ -4,11 +4,12 @@ from django.conf.urls import patterns, include, url
 # from django.contrib import admin
 # admin.autodiscover()
 
-from quize.views import main_page
+from quize.views import main_page, logout_page
 
 urlpatterns = patterns('',
     (r'^$', main_page),
     (r'^login/$', 'django.contrib.auth.views.login'),
+    (r'^logout/$', logout_page),
     # Examples:
     # url(r'^$', 'oquize.views.home', name='home'),
     # url(r'^oquize/', include('oquize.foo.urls')),
--- quize/views.py
+++ quize/views.py
@@ -1,6 +1,10 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.shortcuts import render, render_to_response
+from django.contrib.auth.views import logout
 
 def main_page(request):
-    return render_to_response("quize/main_page.html")
-    
+    return render(request, "quize/main_page.html")
+
+def logout_page(request):
+    logout(request)
+    return HttpResponseRedirect("/")
