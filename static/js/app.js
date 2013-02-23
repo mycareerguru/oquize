0cf3ba918fcc19cdbddba98bd3b69907dcbb0340
Sagar R. Gosavi <sagargosavi1112@gmail.com>
2013-02-24 00:03:27 +0530

    Added missig app.js

---
+++ static/js/app.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+
+    $(".search-query").keydown(function(e) {
+        if (e.which == 13) {
+            query = $(".search-query").val();
+            console.log(query);
+            url = "/search1/?query=" + encodeURIComponent(query);
+            console.log(url);
+            $("#container").load(url, function() {
+                $("#container").children().first().before("<h3>Search result for " + $(".search-query").val() + "</h3>")
+                console.log("AJAX success");
+            });
+            console.log("Enter button pressed", "value of search is ", $(".search-query").val());
+            e.preventDefault();
+        }
+    })
+})
+
+
