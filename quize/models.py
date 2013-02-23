65ff9a4c50bb3b4779b9b9fb92c60e9f2dd1fa9f
Sagar R. Gosavi <sagargosavi1112@gmail.com>
2013-02-23 16:17:19 +0530

    Added model for aptitude quize

--- quize/models.py
+++ quize/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Question(models.Model):
+    date_added = models.DateField(auto_now_add=True)
+    user = models.ForeignKey(User)
+    text = models.TextField()
+    opt1 = models.TextField()
+    opt2 = models.TextField()
+    opt3 = models.TextField()
+    opt4 = models.TextField()
+    ans = models.IntegerField()
+    num_attemplted = models.IntegerField(default=0)
+    num_correct = models.IntegerField(default=0)
+    num_likes = models.IntegerField(default=0)
+    num_unlikes = models.IntegerField(default=0)
+       
+class Tag(models.Model):
+    name = models.CharField(max_length=30,unique=True)
+    questions = models.ManyToManyField(Question)
+    
+    def __unicode__(self):
+        return self.name
