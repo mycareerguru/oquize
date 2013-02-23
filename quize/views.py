9e17fcca5509c1444dcd988b996ede9d7d7ddb8b
Sagar R. Gosavi <sagargosavi1112@gmail.com>
2013-02-23 16:18:33 +0530

    Add user page

--- quize/views.py
+++ quize/views.py
@@ -3,33 +3,14 @@ from django.shortcuts import render, render_to_response
 from django.contrib.auth.views import logout
 from django.contrib.auth.models import User
 from quize.forms import RegisterForm
+from quize.models import Question
 
 def main_page(request):
     return render(request, "quize/main_page.html")
 
-def register_page(request):
-    form = RegisterForm()
+def user_page(request, user):
+    questions = Question.objects.all()
+    return render(request, "quize/user_page.html", {
+        'questions' : questions
+    })
     
-    if request.method == "POST":
-        form = RegisterForm(request.POST)
-        if form.is_valid():
-           # save user
-           user = User.objects.create_user(
-            username = form.cleaned_data['username'],
-            password = form.cleaned_data['password1'],
-            email = form.cleaned_data['email']
-           )
-           
-           # and redirect to /registration_sucess
-           return HttpResponseRedirect("/register/success")
-        
-    return render(request, "registration/register.html", {
-        'form' : form
-        })
-        
-def register_success(request):
-    return render(request, "registration/register_success.html")
-          
-def logout_page(request):
-    logout(request)
-    return HttpResponseRedirect("/")
