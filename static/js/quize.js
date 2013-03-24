d0c2e54f987ffb07610c0ea001b4f1ec8754e392
Sagar R. Gosavi <sagargosavi1112@gmail.com>
2013-03-24 20:44:17 +0530

    javascript code handle quiz interface

---
+++ static/js/quize.js
@@ -0,0 +1,31 @@
+
+/*   Class for managing quize state */
+function QuizeState(id, total) {
+    this.id = id;
+    this.totalQuestions = total;
+    this.answered = 0;
+    return this;
+}
+
+QuizeState.prototype.questionAnswered = function(q) {
+    this.answered += 1;
+    var perc = (this.answered / this.totalQuestions) * 100;
+    $("#progress .bar").css({
+        'width' : perc + "%",
+    });
+    if (this.answered === this.totalQuestions) {
+        window.location.href = "/quiz/" + this.id;
+    }
+    var next = $(".question")[this.answered];
+    $(next).show();
+    $("#totalA").html(this.answered);
+}
+
+App.questionSubmitted = function(q) {
+    App.quize.questionAnswered(q);
+}
+
+$(function() {
+   $(".question").hide();
+   $(".question").first().show();
+});
