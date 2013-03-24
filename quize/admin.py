835dad3dc4a26c2c73f0a965f356b7103ea81940
Sagar R. Gosavi <sagargosavi1112@gmail.com>
2013-03-24 18:28:01 +0530

    Add Quize and UserQuiz models to admin

--- quize/admin.py
+++ quize/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from quize.models import Question, Quize, Tag
+from quize.models import Question, Quize, Tag, UserQuize, QuizeAnswers
 
 class QuestionAdmin(admin.ModelAdmin):
     list_display = ('user', 'text', 'date_added')
@@ -15,5 +15,13 @@ admin.site.register(Question, QuestionAdmin)
 class QuizeAdmin(admin.ModelAdmin):
     filter_horizontal = ('questions',)
 
+class QuizeAnswerAdmin(admin.ModelAdmin):
+    list_display = ('quize', "question", "ans", "correct")
+
+class UserQuizeAdmin(admin.ModelAdmin):
+    list_display = ("user", "quize")
+
 admin.site.register(Quize, QuizeAdmin)
 admin.site.register(Tag)
+admin.site.register(UserQuize, UserQuizeAdmin)
+admin.site.register(QuizeAnswers, QuizeAnswerAdmin)
